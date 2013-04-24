b2eaf9593ef0c52a06f82fb164e8ba6c69cd71e6
marcelo <marcelogaguero@hotmail.com>
2013-04-24 13:47:14 -0300

    tarea para la creacion de modulos

--- core/tasks/CreateModuleTask.php
+++ core/tasks/CreateModuleTask.php
@@ -30,13 +30,67 @@ class CreateModuleTask extends TaskBase
 
     public function execute($parameters, $options)
     {
-        $path = __DIR__ . "/../../../../modules/".$parameters[0];
+        $module = $parameters[0];
+        $path = __DIR__ . "/../../modules/".strtolower($module);
 
-        if(!mkdir($path."/controllers", 0, true)) throw new \Exception("No se pudo crear el modulo");
-        if(!mkdir($path."/css", 0, true)) throw new \Exception("No se pudo crear el modulo");
-        if(!mkdir($path."/img", 0, true)) throw new \Exception("No se pudo crear el modulo");
-        if(!mkdir($path."/js", 0, true)) throw new \Exception("No se pudo crear el modulo");
-        if(!mkdir($path."/views", 0, true)) throw new \Exception("No se pudo crear el modulo");
+        if(!mkdir($path, 0775, true)) throw new \Exception("No se pudo crear el directorio $path. \n");
+        if(!mkdir($path."/controllers", 0775, true)) throw new \Exception("No se pudo crear el directorio $path/controllers. \n");
+        if(!mkdir($path."/css", 0775, true)) throw new \Exception("No se pudo crear el directorio $path/css. \n");
+        if(!mkdir($path."/img", 0775, true)) throw new \Exception("No se pudo crear el directorio $path/img. \n");
+        if(!mkdir($path."/js", 0775, true)) throw new \Exception("No se pudo crear el directorio $path/js. \n");
+        if(!mkdir($path."/views", 0775, true)) throw new \Exception("No se pudo crear el directorio $path/views. \n");
+
+        $file = $path."/controllers/".$module."Controller.php";
+        $code = $this->getClassCode($module);
+        if (!file_put_contents($file, $code)) die("Error: no se pudo crear el archivo $file. \n");
+
+        $html = $this->getCodeView();
+        $file = $path."/views/index.php";
+        if (!file_put_contents($file, $html)) die("Error: no se pudo crear el archivo $file. \n");
 
     }
+
+    protected function getClassCode($module){
+
+        $dir = strtolower($module);
+        $date = date('d/m/Y m:s');
+        $nameclass = $module."Controller";
+
+        $code = <<<EOD
+<?php
+    /**
+    * @author: Marcelo Agüero <marcelogaguero@hotmail.com>
+    * @since: $date
+    *
+    * Controlador creado dinamicamente con una tarea
+    */
+    namespace modules\\$dir\\controllers;
+
+    use \core\BaseController;
+
+    class $nameclass extends BaseController
+    {
+        public function indexAction(){
+            return \$this->render(\$this->getPathBaseModule().'/views/index.php', array());
+        }
+    }
+EOD;
+        return $code;
+    }
+
+    protected function getCodeView(){
+        $html = <<<EOD
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+        <head>
+        </head>
+        <body>
+            CONTENIDO
+        </body>
+        </html>
+EOD;
+        return $html;
+    }
 }
+
+
